f638be28b64122c94d450b0d6def06102528a9a3
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-01-05 02:37:51 -0300

    feat: add client component.ts

--- src/app/UserManagement/components/clientmanagement/clientmanagement.component.ts
+++ src/app/UserManagement/components/clientmanagement/clientmanagement.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Clients } from '../../interfaces/Clients';
+import { ClientmanagementService } from '../../services/clientmanagement.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-clientmanagement',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './clientmanagement.component.html',
-  styleUrl: './clientmanagement.component.css'
+  styleUrls: ['./clientmanagement.component.css']
 })
-export class ClientmanagementComponent {
+export class ClientmanagementComponent implements OnInit {
+  clients: Clients[]= [];
+  currentPage: number = 1;
+  totalPages: number = 1;
+  searchQuery: string = '';
 
+
+  constructor(private clientService: ClientmanagementService){}
+
+
+  ngOnInit(): void {
+      this.loadClients();
+  }
+
+  loadClients():void{
+    this.clientService.getClients(this.searchQuery, this.currentPage).subscribe({next:(data)=>
+    {
+      this.clients = data.clientes;
+      this.totalPages = data.totalPages;
+    },
+    error:(err)=>console.error('Error al cargar clientes: ', err),
+    });
+  }
+
+  onSearch(event:Event): void {
+    const input = event.target as HTMLInputElement;
+    this.searchQuery = input.value.trim();
+    this.currentPage = 1;
+    this.loadClients();
+  }
+
+  onPageChange(direction:boolean): void{
+    if(direction && this.currentPage < this.totalPages){
+      this.currentPage++;
+    }
+    else if(!direction && this.currentPage > 1){
+      this.currentPage--;
+    }
+  }
+  toggleClienteStatus(clienteId: number, status: boolean): void {
+    this.clientService.toggleClientStatus(clienteId, status).subscribe({
+      next: () => {
+        alert(
+          `Cliente ${status ? 'habilitado' : 'deshabilitado'} con éxito.`
+        );
+        this.loadClients();
+      },
+      error: (err) => console.error('Error al actualizar cliente:', err),
+    });
+  }
 }
