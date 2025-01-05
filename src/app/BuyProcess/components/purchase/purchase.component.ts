23798767b11de57267abbf0465f212b1d52a708e
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-01-05 09:22:48 -0300

    feat: create delivery and purchase components, services

---
+++ src/app/BuyProcess/components/purchase/purchase.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase',
+  imports: [],
+  templateUrl: './purchase.component.html',
+  styleUrl: './purchase.component.css'
+})
+export class PurchaseComponent {
+
+}
