6d9d855f6e2a056e71511e13debddc08474de58c
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-01-05 12:18:33 -0300

    fix: App component and config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
