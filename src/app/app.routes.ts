918c14d6fc5137d69354b50c676943e7a2f663b8
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-01-05 14:32:07 -0300

    feat: Add user sevice and add app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'change-password', component: ChangePasswordComponent },
+];
+
