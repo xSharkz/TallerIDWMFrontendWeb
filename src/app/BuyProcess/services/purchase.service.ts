5d812a40a423d80c6aba25f0363d8b79f6ffb145
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-01-05 11:19:28 -0300

    feat: change delivery to purchase

--- src/app/BuyProcess/services/purchase.service.ts
+++ src/app/BuyProcess/services/purchase.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PurchaseService {
+  private apiUrl= 'http://localhost:5220/api/Order';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
+    confirmOrder(order: { deliveryAddress: string; orderItems: any[]; totalAmount: number }): Observable<any> {
+      return this.http.post(`${this.apiUrl}/orders`, order);
+    }
+
+    getOrders(): Observable<any> {
+      return this.http.get(`${this.apiUrl}/orders`);
+    }
 }
