01d250216bed9f4e77f725c62735f30c251733e6
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-01-05 22:44:58 -0300

    feat: Add path and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { EditProfileComponent } from "./edit-profile/edit-profile.component";
 import { ChangePasswordComponent } from "./change-password/change-password.component";
 import { FormsModule } from '@angular/forms';
 import { ProductManagementComponent } from "./product-management/product-management.component";
+import { SalesViewComponent } from './sales-view/sales-view.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [ProductManagementComponent],
+  imports: [ProductManagementComponent, SalesViewComponent, EditProfileComponent, ChangePasswordComponent, RouterOutlet, FormsModule],
 })
 export class AppComponent {
   title = 'TallerIDWMFrontendWeb';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import { ProductManagementComponent } from './product-management/product-management.component';
+import { SalesViewComponent } from './sales-view/sales-view.component';
 
 export const routes: Routes = [
   { path: 'edit-profile', component: EditProfileComponent },
   { path: 'change-password', component: ChangePasswordComponent },
+  { path: 'product-management', component: ProductManagementComponent },
+  { path: 'sales-view', component: SalesViewComponent },
 ];
 
