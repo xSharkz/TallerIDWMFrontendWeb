ab229118ef687696f56e02a142965c0909f4c5d1
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-01-05 21:00:45 -0300

    fix: app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EditProfileComponent } from "./edit-profile/edit-profile.component";
+import { ChangePasswordComponent } from "./change-password/change-password.component";
+import { FormsModule } from '@angular/forms';
+import { ProductManagementComponent } from "./product-management/product-management.component";
 
 @Component({
   selector: 'app-root',
-  imports: [EditProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ProductManagementComponent],
 })
 export class AppComponent {
   title = 'TallerIDWMFrontendWeb';
